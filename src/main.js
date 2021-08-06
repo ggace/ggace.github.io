70f21b83d415f361e1fb71062af8297ba7ab2f61
ggace <skydream5485@gmail.com>
2021-08-07 01:11:54 +0900

    이미지 작업

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@
 const container = document.getElementById('container')
 const content = document.getElementById('content')
 
+
+
 const elts = {
 	text1: document.getElementById("text1"),
 	text2: document.getElementById("text2")
@@ -12,6 +14,7 @@ const elts = {
 
 // The strings to morph between. You can change these to anything you want!
 let texts = [];
+let imgsComponent = PluginArray;
 
 // Controls the speed of morphing.
 const morphTime = 1;
@@ -26,6 +29,14 @@ let cooldown = cooldownTime;
 
 let containerHeight = 100;
 let contentHeight = 0;
+
+let imgs = [
+	"네이버.png",
+	"kakao사옥.jpeg",
+	"ncsoft.png",
+	"nexon.png",
+	"판교it.jpg"
+]
 
 function doMorph() {
 	morph -= cooldown;
@@ -66,8 +77,73 @@ function doCooldown() {
 	elts.text1.style.opacity = "0%";
 }
 
-function doAct(){
-  let newTime = new Date();
+function setImgs(){
+	let div = <div id="imgs">
+		{
+			imgs.map((img, index)=> (
+				<Img src={`../sources/${img}`}/>
+			))
+		}
+		
+	</div>
+	
+	ReactDOM.render(div, content);
+	setTimeout(()=>{console.log(""); setSize(0, true)}, 3000);
+
+	
+	//;
+	
+}
+let objHeight = 0;
+let imgObj = undefined;
+function setSize(index, is_up){
+	imgObj = document.querySelectorAll("#imgs img")[index];
+	if(is_up){
+		objHeight = 0;
+		setSizeUp(index);
+	}
+	else{
+		
+		objHeight = 100;
+		setSizeDown(index);
+	}
+
+	
+}
+
+function setSizeUp(){
+	let sizeUp = requestAnimationFrame(setSizeUp);
+	
+	
+	
+	if(objHeight >= 100){
+		cancelAnimationFrame(sizeUp)
+		return 0;
+	}
+	imgObj.style.height = (objHeight+2).toString() + "%";
+	objHeight = objHeight+5;
+}
+
+function setSizeDown(index){
+
+	let sizeDown = requestAnimationFrame(setSizeDown);
+
+	let tempObj = document.getElementById("imgs")[index];
+	let obj = tempObj.getElementsByTagName("img")[0];
+
+	if(objHeight <= 0){
+		cancelAnimationFrame(sizeDown)
+		return 0;
+	}
+	obj.style.height = (objHeight-2).toString() + "%";
+	objHeight = objHeight-5;
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+	let animateAnimation = requestAnimationFrame(animate);
+	
+	let newTime = new Date();
 	let shouldIncrementIndex = cooldown > 0;
 	let dt = (newTime - time) / 1000;
 	time = newTime;
@@ -79,20 +155,19 @@ function doAct(){
 			textIndex++;
 		}
 		if(textIndex % texts.length == texts.length-1){
-			setTimeout(setHeightContainer(), 10000);
+			setTimeout(() => {
+				setImgs()
+				
+				setHeightContainer();
+				
+			}, 2000);
+			cancelAnimationFrame(animateAnimation)
 			return 0;
 		}
 		doMorph();
 	} else {
 		doCooldown();
 	}
-}
-
-// Animation loop, which is called every frame.
-function animate() {
-	requestAnimationFrame(animate);
-	
-	doAct();
 
 }
 
@@ -120,7 +195,7 @@ function begin(showTexts = []){
   animate()
 }
 
-let plus = 0.2;
+let plus = 3;
 
 function setDisplay(){
 	
@@ -129,13 +204,16 @@ function setDisplay(){
 	document.body.style.color="black";
 	container.style.backgroundColor="black"
 	container.style.color="white"
+
+	
 }
 
 function setHeightContainer() {
-	requestAnimationFrame(setHeightContainer);
+	let heightContainer = requestAnimationFrame(setHeightContainer);
 
 	if(containerHeight <= 10){
 		setDisplay();
+		cancelAnimationFrame(heightContainer)
 		return 0;
 	}
 	container.style.height = (containerHeight-plus).toString() + "%";
@@ -145,16 +223,23 @@ function setHeightContainer() {
 	contentHeight += plus;
 }
 
-class Img extends React.Component {
-	render(){
-	  
-	  return (
-		<img href={this.props.href}>{this.props.children}</img>
-	  );
-	}
-  }
 
-  ReactDOM.render(<Img href="../sources/네이버.png">네이버</Img>, content);
+class Img extends React.Component{
+	render() {
+		const divStyle = {
+			"width" : "100%",
+			"height": "100%",
+			"justifyContent" : "center",
+			"alignItems": "flex-end",
+			"display":"flex"
+			
+		};
+		const imgStyle={
+			"height": "0%"
+		}
+		return <div style={divStyle}><img src={this.props.src} style={imgStyle} /></div>
+	}
+}
 // Start the animation.
 begin([
 	"Post Corona-19", "Tour Guide", "Mini Book", "to", "Pangyo-dong,", "Bundang-gu,", "Seongnam-si", "with My Dream,", "Programmer", "Tour Guide to Pangyo-dong"
